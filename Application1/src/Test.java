b6d458027fb81b8fee1d6cff49a240e74c41dfa0
AakulSaiKiran <aakulasaikiran@gmail.com>
2018-07-16 22:09:26 +0530

    commit2

--- Application1/src/Test.java
+++ Application1/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		String[] s = { "1", "2", "3", "4", "5" };
+		String[] s = { "1", "2", "3", "4", "5","6" };
 		for (int i = 0; i < s.length; i++) {
 			System.out.println(s[i]);
 		}

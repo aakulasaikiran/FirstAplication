2456db6476c282e13581de807cafa35744e7cafd
aakulasaikiran1 <aakulasaikiran@gmail.com>
2018-07-16 21:56:49 +0530

    commit1

---
+++ Application1/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		String[] s = { "1", "2", "3", "4", "5" };
+		for (int i = 0; i < s.length; i++) {
+			System.out.println(s[i]);
+		}
+
+	}
+
+}

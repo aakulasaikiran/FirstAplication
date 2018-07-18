f09f7214420c7dbca9494edfd809cc459af7e10d
aakulasaikiran <aakulasaikiran@gmail.com>
2018-07-18 09:29:03 +0530

    20 added

--- Application1/src/Test.java
+++ Application1/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		String[] s = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+		String[] s = { "1", "2", "3", "4", "5", "6", "7", "8", "9","20" };
 		for (int i = 0; i < s.length; i++) {
 			System.out.print(s[i] + " ");
 		}
